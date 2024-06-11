4fdbde0cd58b00399c9f54cc426c91ece713124f
User <User@W11PC>
2024-06-11 17:51:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static AutoResetEvent events = new AutoResetEvent(false);
+    //static ManualResetEvent events = new ManualResetEvent(false);
+
+    static void Main()
+    {
+        Thread thread1 = new Thread(ThreadWork);
+        thread1.Start();
+
+        Console.WriteLine("Main thread does some work, then signals.");
+        //Thread.Sleep(1000);
+
+        events.Set();
+
+        Console.WriteLine("Main thread waits for thread to complete.");
+        thread1.Join();
+
+        Console.WriteLine("Main thread exits.");
+    }
+
+    static void ThreadWork()
+    {
+        Console.WriteLine("Thread waits for signal.");
+        events.WaitOne();
+        Console.WriteLine("Thread received signal and continues.");
+    }
+}
